e19ed29da7dee5d74b9a70386e4aa05092c1b970
Mek <nik.podterob@gmail.com>
2023-03-05 18:03:44 +0300

    Initial Commit

---
+++ Edit_distance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Edit_distance
+//
+//  Created by Никита Подтероб on 5.03.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
